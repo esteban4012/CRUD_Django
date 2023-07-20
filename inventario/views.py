9e8e9c9b2938e9ed116ac756657092c2c2ab4746
esteban <esteban4012223@gmail.com>
2023-07-20 11:13:11 -0500

    creamos la vista home para ver todos los objetos creados

--- inventario/views.py
+++ inventario/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Articulo
 
 # Create your views here.
+
+def home(request):
+    articulo = Articulo.objects.all()
+    return render(request, "inventario/home.html", {"articulo" : articulo})
