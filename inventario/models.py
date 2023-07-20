e45c5e82fa13402a51ae57e97bd61cd95d569b8a
esteban <esteban4012223@gmail.com>
2023-07-20 10:56:48 -0500

    agregamos la clase Articulo con sus atributos y su metodo str

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Articulo(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return f"{self.id}.{self.descripcion}  {self.precio}"
