96ab2fc83592f6becc9c69ff69994aa6a8b79968
esteban <esteban4012223@gmail.com>
2023-07-20 16:58:07 -0500

    Agregando una ruta para eliminar un artículo

--- inventario/urls.py
+++ inventario/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home),
-    path("registrarArticulo/", views.registrar_articulo)
+    path("registrarArticulo/", views.registrar_articulo),
+    path("eliminarArticulo/<id>", views.eliminarArticulo)
 ]
