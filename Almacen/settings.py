a4a998a1da3c2b0f64862432447e43dc084bdcbd
esteban <esteban4012223@gmail.com>
2023-07-21 09:19:41 -0500

    configurando archivos settings.py conectandome a la base de datos postgresql

--- Almacen/settings.py
+++ Almacen/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Almacen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'prueba_django',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
